ee39946834d98647325927b48afcee274d094dcf
coder439 <65747567+coder439@users.noreply.github.com>
2020-05-26 11:57:45 -0500

    Update tutorRegistration.php
    fixed button name - changed to register

--- Volunteer/Scholastician/Documents/tutorRegistration.php
+++ Volunteer/Scholastician/Documents/tutorRegistration.php
@@ -8,35 +8,23 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!--====== Title ======-->
     <title>Tutor Registration</title>
-
     <!--====== Favicon Icon ======-->
     <link rel="shortcut icon" href="assets/images/favicon.jpg" type="image/jpg">
-
     <!--====== Bootstrap css ======-->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
     <!--====== Line Icons css ======-->
     <link rel="stylesheet" href="assets/css/LineIcons.css">
-
     <!--====== Magnific Popup css ======-->
     <link rel="stylesheet" href="assets/css/magnific-popup.css">
-
     <!--====== Default css ======-->
     <link rel="stylesheet" href="assets/css/default.css">
-
     <!--====== Style css ======-->
     <link rel="stylesheet" href="assets/css/style.css">
-
-
 </head>
-
 <body>
-
     <!--====== PRELOADER PART START ======-->
-
     <div class="preloader">
         <div class="loader_34">
             <div class="ytp-spinner">
@@ -53,11 +41,8 @@
             </div>
         </div>
     </div>
-
     <!--====== PRELOADER ENDS START ======-->
-
     <!--====== HEADER PART START ======-->
-
     <header id="home" class="header-area">
         <div class="navigation fixed-top">
             <div class="container">
@@ -73,10 +58,7 @@
                 </div> <!-- row -->
             </div> <!-- container -->
         </div> <!-- navigation -->
-
-
     </header>
-
     <section id="contact" class="contact-area pt-125 pb-130 white-bg">
         <div class="container">
             <div class="row justify-content-center mt-50">
@@ -87,7 +69,6 @@
                     </div> <!-- section title -->
                 </div>
             </div> <!-- row -->
-
             <div class="row justify-content-center">
                 <div class="col-lg-8">
                     <div class="contact-form pt-30">
@@ -100,7 +81,6 @@
                               <h5 class="about-title">Email Address</h5>
                                 <input type="email" name="email" placeholder="Email">
                             </div> <!-- single form -->
-
                             <div class="single-form">
                               <h5 class="about-title">Mobile Phone Number</h5>
                                 <input type="number" name="phone_number" placeholder="Phone Number">
@@ -121,23 +101,17 @@
                               <h5 class="about-title">ACT Score(If Applicable)</h5>
                                 <input type="number" name="ACT" placeholder="ACT Score">
                             </div>
-
-
-
                             <p class="form-message">Please ensure all content is filled and accurate before submitting</p>
                             <div class="single-form">
-                                <button class="main-btn" type="submit">Send Message</button>
+                                <button class="main-btn" type="submit">Register</button>
                             </div> <!-- single form -->
                         </form>
                     </div> <!-- contact form -->
                 </div>
-
             </div> <!-- row -->
         </div> <!-- container -->
     </section>
-
     <!--====== FOOTER PART START ======-->
-
     <footer id="footer" class="footer-area">
         <div class="footer-widget pt-130 pb-130">
             <div class="container">
@@ -171,51 +145,29 @@
             </div> <!-- container -->
         </div> <!-- footer widget -->
     </footer>
-
     <!--====== FOOTER PART ENDS ======-->
-
     <!--====== BACK TOP TOP PART START ======-->
-
     <a href="#" class="back-to-top"><i class="lni-chevron-up"></i></a>
-
     <!--====== BACK TOP TOP PART ENDS ======-->
-
-
-
-
-
-
-
     <!--====== jquery js ======-->
     <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
     <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
-
     <!--====== Bootstrap js ======-->
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/popper.min.js"></script>
-
     <!--====== Magnific Popup js ======-->
     <script src="assets/js/jquery.magnific-popup.min.js"></script>
-
     <!--====== Parallax js ======-->
     <script src="assets/js/parallax.min.js"></script>
-
     <!--====== Counter Up js ======-->
     <script src="assets/js/waypoints.min.js"></script>
     <script src="assets/js/jquery.counterup.min.js"></script>
-
-
     <!--====== Appear js ======-->
     <script src="assets/js/jquery.appear.min.js"></script>
-
     <!--====== Scrolling js ======-->
     <script src="assets/js/scrolling-nav.js"></script>
     <script src="assets/js/jquery.easing.min.js"></script>
-
-
     <!--====== Main js ======-->
     <script src="assets/js/main.js"></script>
-
 </body>
-
 </html>
