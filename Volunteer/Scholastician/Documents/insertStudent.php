cf794d45c321168bfb51658be98131a550ddec65
coder439 <65747567+coder439@users.noreply.github.com>
2020-05-30 21:35:16 -0500

    username uniqueness, need to fix alert
    alert capability not yet functional, alert is not displaying

--- Volunteer/Scholastician/Documents/insertStudent.php
+++ Volunteer/Scholastician/Documents/insertStudent.php
@@ -15,6 +15,47 @@ $grade = mysqli_real_escape_string($link, $_REQUEST['grade']);
 $user = mysqli_real_escape_string($link, $_REQUEST['username']);
 $pass = mysqli_real_escape_string($link, $_REQUEST['password']);
 
+
+//check if username already exists
+$sql = "SELECT  username  FROM userinfo WHERE username = ?";
+
+ if($stmt = mysqli_prepare($link, $sql))
+ {
+   //echo "print 1";
+   // Bind variables to the prepared statement as parameters
+              mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+              // Set parameters
+              $param_username = $user;
+          //    echo   $param_username;
+
+
+  // Attempt to execute the prepared statement
+          if(mysqli_stmt_execute($stmt))
+
+          {
+          //  echo "print 2";
+
+              // Store result
+
+              mysqli_stmt_store_result($stmt);
+
+              // Check if username exists, if yes then verify password
+                 if(mysqli_stmt_num_rows($stmt) >= 1)
+
+                 {
+
+                      header("Location:userRegistration.php");
+                      echo "<script type='text/javascript'>alert('username taken');</script>";
+                      exit();
+                   }
+          }
+  }
+
+
+
+
+
 // Attempt insert query execution
 $sql = "INSERT INTO userInfo (Name, Email, PN, Grade, Username, Password, student) VALUES ('$name', '$email','$phone', '$grade', '$user', '$pass', '1')";
 if(mysqli_query($link, $sql)){
