521b41cc18904caa0116ca034b72121946eac162
ArkhamKnight9103 <nihal.bhatnagar@gmail.com>
2020-06-03 14:59:42 -0500

    gg

--- account (5).sql
+++ account (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2020 at 01:04 AM
+-- Generation Time: Jun 03, 2020 at 09:58 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `userinfo` (
+  `id` int(11) NOT NULL,
   `Name` varchar(100) NOT NULL,
   `Username` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
@@ -55,21 +56,24 @@ CREATE TABLE `userinfo` (
   `apecon` tinyint(1) NOT NULL,
   `english1` tinyint(1) NOT NULL,
   `english2` tinyint(1) NOT NULL,
+  `english3` tinyint(1) NOT NULL,
+  `english4` tinyint(1) NOT NULL,
   `aplang` tinyint(1) NOT NULL,
   `aplit` tinyint(1) NOT NULL,
   `satclass` tinyint(1) NOT NULL,
   `actclass` tinyint(1) NOT NULL,
   `compsci` tinyint(1) NOT NULL,
-  `matching` tinyint(1) NOT NULL
+  `matching` tinyint(1) NOT NULL,
+  `topics` text NOT NULL,
+  `times` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `userinfo`
 --
 
-INSERT INTO `userinfo` (`Name`, `Username`, `Password`, `Email`, `PN`, `Grade`, `SAT`, `ACT`, `student`, `Algebra1`, `Algebra2`, `Pre-Calc`, `APCalcAB`, `bio`, `chem`, `APCalcBC`, `Physics1`, `Physics2`, `PhysicsC`, `apes`, `hgap`, `whap`, `apush`, `apgov`, `apecon`, `english1`, `english2`, `aplang`, `aplit`, `satclass`, `actclass`, `compsci`, `matching`) VALUES
-('Nihal Bhatnagar', 'nihal', '1234', 'nihal.bhatnagar@gmail.com', '9726181919', 11, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('test1', 'test1', 'test1', 'test1@gmail.com', '1234512345', 11, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO `userinfo` (`id`, `Name`, `Username`, `Password`, `Email`, `PN`, `Grade`, `SAT`, `ACT`, `student`, `Algebra1`, `Algebra2`, `Pre-Calc`, `APCalcAB`, `bio`, `chem`, `APCalcBC`, `Physics1`, `Physics2`, `PhysicsC`, `apes`, `hgap`, `whap`, `apush`, `apgov`, `apecon`, `english1`, `english2`, `english3`, `english4`, `aplang`, `aplit`, `satclass`, `actclass`, `compsci`, `matching`, `topics`, `times`) VALUES
+(1, 'Nihal Bhatnagar', 'nihal', '1234', 'nihal.bhatnagar@gmail.com', '9726181919', 11, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 'gg', 'wp');
 
 --
 -- Indexes for dumped tables
@@ -79,7 +83,20 @@ INSERT INTO `userinfo` (`Name`, `Username`, `Password`, `Email`, `PN`, `Grade`, 
 -- Indexes for table `userinfo`
 --
 ALTER TABLE `userinfo`
-  ADD UNIQUE KEY `Username` (`Username`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Username` (`Username`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
