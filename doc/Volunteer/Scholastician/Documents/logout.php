fd6fceed9915b1ad682868cdd9b29b5cce510c88
thabat12 <65791148+thabat12@users.noreply.github.com>
2020-06-04 22:13:54 -0500

    deleted

--- doc/Volunteer/Scholastician/Documents/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Destroying the session clears the $_SESSION variable, thus "logging" the user
-// out. This also happens automatically when the browser is closed
-session_destroy();
-header("Location: index.php");
-?>
